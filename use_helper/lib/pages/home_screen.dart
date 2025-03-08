18ec071dfabf14ffea6e599ac0e283ab330c39d1
themooss <themossunder@gmail.com>
2025-03-08 23:40:26 +0300

    adding files

---
+++ use_helper/lib/pages/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: Text(
+          "Hello, World",
+          style: TextStyle(color: Colors.amberAccent, fontSize: 26),
+        ),
+      )),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
