cc9e8a8e44ad24ac0e3ee62d492e35b32716496d
themooss <themossunder@gmail.com>
2025-03-20 12:55:27 +0300

    create a dialog menu on app start

--- use_helper/lib/main.dart
+++ use_helper/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:use_helper/pages/home_screen.dart';
+import 'package:use_helper/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
